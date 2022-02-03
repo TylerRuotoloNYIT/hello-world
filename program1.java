a7d04ddb139f8aeba51fc17811de728f084f3bba
TylerRuotoloNYIT <98786846+TylerRuotoloNYIT@users.noreply.github.com>
2022-02-03 09:12:49 -0500

    program1
    a simple program that prints information and adds numbers

---
+++ program1.java
@@ -0,0 +1,19 @@
+class Testing{
+    public static void main(String[] args) {
+        String studentName = "Tyler Ruotolo";
+        String studentEmail = "truotolo@nyit.edu";
+        String studentID = "1298112";
+        int num1 = 3;
+        int num2 = 5;
+        int num3 = num1+num2;
+        System.out.println("Name: " + studentName);
+        System.out.println("Email: " + studentEmail);
+        System.out.println("ID: " + studentID);
+        System.out.println(num1 + " + " + num2 + " = " + num3);
+        System.out.println(num3 + " + " + 1 + " = " + num3+1);
+    }
+}
+
+//Tyler Ruotolo
+//February 3rd, 2022
+//Computer Programming I
